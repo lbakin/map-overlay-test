01451c536b5b27b01a9d94c2da76b3a47bc5a61b
Lev Bakin <lev@onapproval.com>
2025-06-21 11:56:37 -0700

    fixing more build errors and warnings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import CustomMap from './CustomMap';
+
 import OperationsMap from './OperationsMap';
 
 function App() {
   return (
     <div className="App">
-      {/* <CustomMap /> */}
+      
       <OperationsMap />
     </div>
   );
